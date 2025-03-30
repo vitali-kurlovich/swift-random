3670ef7604bac973ad45b367934f6d21af12b9fb
Vitali Kurlovich <vitalikurlovich@gmail.com>
2025-03-30 23:53:28 +0400

    Update Benchmark

--- Sources/Benchmarks/Benchmark.swift
+++ Sources/Benchmarks/Benchmark.swift
@@ -18,19 +18,19 @@ struct Benchmark: ParsableCommand {
         benchmark.benchmark(name: "CryptoRandomGenegator") { context in
             var generator = CryptoRandomGenegator(seed: 0)
             for _ in 0 ..< 10_000_000 {
-                _ = generator.next()
+                _ = UInt64.random(in: UInt64.min ... UInt64.max, using: &generator)
             }
 
-            context.blackHole(generator.next())
+            context.blackHole(UInt64.random(in: UInt64.min ... UInt64.max, using: &generator))
         }
 
         benchmark.benchmark(name: "FastRandomGenegator") { context in
             var generator = FastRandomGenegator()
             for _ in 0 ..< 10_000_000 {
-                _ = generator.next()
+                _ = UInt64.random(in: UInt64.min ... UInt64.max, using: &generator)
             }
 
-            context.blackHole(generator.next())
+            context.blackHole(UInt64.random(in: UInt64.min ... UInt64.max, using: &generator))
         }
 
         benchmark.benchmark(name: "SwiftRandomGenegator") { context in
