62d3f1a46fee91071885ae73455bcbaf81e749c1
Vitali Kurlovich <vitalikurlovich@gmail.com>
2025-03-30 22:23:43 +0400

    swift-random

---
+++ Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-random",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Random",
+            targets: ["Random"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.8.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/vitali-kurlovich/Benchmarks", from: "0.1.1"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Random",
+            dependencies: [
+                .product(name: "Crypto", package: "swift-crypto"),
+            ]
+        ),
+        .executableTarget(
+            name: "RandomGeneratorBenchmarks",
+
+            dependencies: [
+                "Random",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "Benchmarks",
+            ],
+            path: "Sources/Benchmarks"
+        ),
+        .testTarget(
+            name: "RandomTests",
+            dependencies: [
+                "Random",
+            ]
+        ),
+    ]
+)
