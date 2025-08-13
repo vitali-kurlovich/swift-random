e306b8268d3071eaa690dcbcfaeb9691c7690f6c
Vitali Kurlovich <vitalikurlovich@gmail.com>
2025-08-13 15:22:45 +0400

    Upd

--- Sources/Random/MT19937/Internal/mt19937_64.swift
+++ Sources/Random/MT19937/Internal/mt19937_64.swift
@@ -5,21 +5,6 @@
 //  Created by Vitali Kurlovich on 11.08.25.
 //
 
-/*
-
- #define NN 312
- #define MM 156
- #define MATRIX_A 0xB5026F5AA96619E9ULL
- #define UM 0xFFFFFFFF80000000ULL /* Most significant 33 bits */
- #define LM 0x7FFFFFFFULL /* Least significant 31 bits */
-
- struct mt19937_64 {
-     unsigned long long mt[NN];
-     size_t mti;
- };
-
- */
-
 @usableFromInline
 struct mt19937_64: Equatable {
     private var mt = ContiguousArray(repeating: UInt64(0), count: .NN)
@@ -34,7 +19,7 @@ extension mt19937_64 {
 
         mt[0] = seed
 
-        while mti < .MM {
+        while mti < .NN {
             mt[mti] = (mt[mti - 1] ^ (mt[mti - 1] >> 62))
                 .multipliedReportingOverflow(by: 6_364_136_223_846_793_005)
                 .partialValue
@@ -56,18 +41,18 @@ extension mt19937_64 {
 
             while i < .NN - .MM {
                 x = (mt[i] & .UM) | (mt[i + 1] & .LM)
-                mt[i] = mt[i + .MM] ^ (x >> 1) ^ magic(x)
+                mt[i] = mt[i + .MM] ^ (x >> 1) ^ ((x & 0x1) * .MATRIX_A)
                 i += 1
             }
 
             while i < .NN - 1 {
                 x = (mt[i] & .UM) | (mt[i + 1] & .LM)
-                mt[i] = mt[i + (.MM - .NN)] ^ (x >> 1) ^ magic(x)
+                mt[i] = mt[i + (.MM - .NN)] ^ (x >> 1) ^ ((x & 0x1) * .MATRIX_A)
                 i += 1
             }
 
             x = (mt[.NN - 1] & .UM) | (mt[0] & .LM)
-            mt[.NN - 1] = mt[.MM - 1] ^ (x >> 1) ^ magic(x)
+            mt[.NN - 1] = mt[.MM - 1] ^ (x >> 1) ^ ((x & 0x1) * .MATRIX_A)
 
             mti = 0
         }
@@ -81,13 +66,6 @@ extension mt19937_64 {
         x ^= (x >> 43)
 
         return x
-    }
-}
-
-extension mt19937_64 {
-    @inline(__always)
-    private func magic(_ x: UInt64) -> UInt64 {
-        (x & 0x1) != 0 ? .MATRIX_A : 0
     }
 }
 
