3f5dbdf15b936242d43772a073659a92cb06e84e
Vitali Kurlovich <vitalikurlovich@gmail.com>
2025-09-18 20:57:35 +0400

    Update test

--- Tests/RandomTests/FaroShuffleTest.swift
+++ Tests/RandomTests/FaroShuffleTest.swift
@@ -36,31 +36,14 @@ struct FaroShuffle {
     }
 
     @Test("Shuffle")
-    func shuffleArrayFaro() {
+    func shuffleArray() {
         var array: [Int] = [1, 2, 3, 4, 5, 6]
         var generator = MT19937RandomGenegator(seed: 42) // false, true, true, false, true, false, true, true, false, true
-        array.shuffle(algorithm: .faro(.init(count: 1)), using: &generator)
-        #expect(array == [4, 1, 5, 2, 6, 3])
 
-        generator = MT19937RandomGenegator(seed: 42)
-        array = [1, 2, 3, 4, 5, 6]
-        array.shuffle(algorithm: .faro(.init(count: 2)), using: &generator)
-        #expect(array == [4, 2, 1, 6, 5, 3])
+        var configuration = FaroShuffleConfiguration(count: 5, middleShiftRange: 0 ... 0, stuckCardsRange: 1 ... 1)
 
-        generator = MT19937RandomGenegator(seed: 42)
         array = [1, 2, 3, 4, 5, 6]
-        array.shuffle(algorithm: .faro(.init(count: 3)), using: &generator)
-        #expect(array == [4, 6, 2, 5, 1, 3])
-
-        generator = MT19937RandomGenegator(seed: 42)
-        array = [1, 2, 3, 4, 5, 6]
-        array.shuffle(algorithm: .faro(.init(count: 4)), using: &generator)
-        #expect(array == [5, 4, 1, 6, 3, 2])
-
-        generator = MT19937RandomGenegator(seed: 42)
-        array = [1, 2, 3, 4, 5, 6]
-        array.shuffle(algorithm: .faro(.init(count: 5)), using: &generator)
-        #expect(array == [5, 6, 4, 3, 1, 2])
+        array.shuffle(algorithm: .faro(configuration), using: &generator)
 
         // [1, 2, 3, 4, 5, 6] : [1, 2, 3] | [4, 5, 6] -> [4, 1, 5, 2, 6, 3]
         // [4, 1, 5, 2, 6, 3] : [4, 1, 5] | [2, 6, 3] -> [4, 2, 1, 6, 5, 3]
@@ -68,35 +51,14 @@ struct FaroShuffle {
         // [4, 6, 2, 5, 1, 3] : [4, 6, 2] | [5, 1, 3] -> [5, 4, 1, 6, 3, 2]
         // [5, 4, 1, 6, 3, 2] : [5, 4, 1] | [6, 3, 2] -> [5, 6, 4, 3, 1, 2]
 
-        generator = MT19937RandomGenegator(seed: 42)
-        array = [1, 2, 3, 4, 5, 6, 7]
-        array.shuffle(algorithm: .faro(.init(count: 1)), using: &generator)
-        #expect(array == [4, 1, 5, 2, 6, 3, 7])
+        #expect(array == [5, 6, 4, 3, 1, 2])
 
         generator = MT19937RandomGenegator(seed: 42)
         array = [1, 2, 3, 4, 5, 6, 7]
-        array.shuffle(algorithm: .faro(.init(count: 2)), using: &generator)
-        #expect(array == [4, 6, 1, 3, 5, 7, 2])
 
-        generator = MT19937RandomGenegator(seed: 42)
-        array = [1, 2, 3, 4, 5, 6, 7]
-        array.shuffle(algorithm: .faro(.init(count: 3)), using: &generator)
-        #expect(array == [4, 5, 6, 7, 1, 2, 3])
+        configuration.count = 6
 
-        generator = MT19937RandomGenegator(seed: 42)
-        array = [1, 2, 3, 4, 5, 6, 7]
-        array.shuffle(algorithm: .faro(.init(count: 4)), using: &generator)
-        #expect(array == [7, 4, 1, 5, 2, 6, 3])
-
-        generator = MT19937RandomGenegator(seed: 42)
-        array = [1, 2, 3, 4, 5, 6, 7]
-        array.shuffle(algorithm: .faro(.init(count: 5)), using: &generator)
-        #expect(array == [7, 2, 4, 6, 1, 3, 5])
-
-        generator = MT19937RandomGenegator(seed: 42)
-        array = [1, 2, 3, 4, 5, 6, 7]
-        array.shuffle(algorithm: .faro(.init(count: 6)), using: &generator)
-        #expect(array == [6, 7, 1, 2, 3, 4, 5])
+        array.shuffle(algorithm: .faro(configuration), using: &generator)
 
         // [1, 2, 3, 4, 5, 6, 7] : [1, 2, 3]    | [4, 5, 6, 7] -> [4, 1, 5, 2, 6, 3, 7]
         // [4, 1, 5, 2, 6, 3, 7] : [4, 1, 5, 2] | [6, 3, 7]    -> [4, 6, 1, 3, 5, 7, 2]
@@ -104,5 +66,23 @@ struct FaroShuffle {
         // [4, 5, 6, 7, 1, 2, 3] : [4, 5, 6]    | [7, 1, 2, 3] -> [7, 4, 1, 5, 2, 6, 3]
         // [7, 4, 1, 5, 2, 6, 3] : [7, 4, 1, 5] | [2, 6, 3]    -> [7, 2, 4, 6, 1, 3, 5]
         // [7, 2, 4, 6, 1, 3, 5] : [7, 2, 4]    | [6, 1, 3, 5] -> [6, 7, 1, 2, 3, 4, 5]
+
+        #expect(array == [6, 7, 1, 2, 3, 4, 5])
+    }
+
+    @Test("Shuffle stuck cards")
+    func shuffleArrayStuckCards() {
+        var generator = MT19937RandomGenegator(seed: 42) // false, true, true, false, true, false, true, true, false, true
+        let configuration = FaroShuffleConfiguration(count: 4, middleShiftRange: 0 ... 0, stuckCardsRange: 2 ... 2)
+
+        var array: [Int] = [1, 2, 3, 4, 5, 6]
+        array.shuffle(algorithm: .faro(configuration), using: &generator)
+
+        // [1, 2, 3, 4, 5, 6] : [1, 2, 3] | [4, 5, 6] -> [4, 5, 1, 2, 6, 3]
+        // [4, 5, 1, 2, 6, 3] : [4, 5, 1] | [2, 6, 3] -> [4, 5, 2, 6, 1, 3]
+        // [4, 5, 2, 6, 1, 3] : [4, 5, 2] | [6, 1, 3] -> [4, 5, 6, 1, 2, 3]
+        // [4, 5, 6, 1, 2, 3] : [4, 5, 6] | [1, 2, 3] -> [1, 2, 4, 5, 3, 6]
+
+        #expect(array == [1, 2, 4, 5, 3, 6])
     }
 }
