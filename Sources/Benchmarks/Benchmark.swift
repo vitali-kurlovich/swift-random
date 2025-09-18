903697e4a840feb4609a596fe92eb87b7529c2ba
Vitali Kurlovich <vitalikurlovich@gmail.com>
2025-09-19 02:08:26 +0400

    Benchmark update

--- Sources/Benchmarks/Benchmark.swift
+++ Sources/Benchmarks/Benchmark.swift
@@ -89,6 +89,18 @@ private extension Benchmark {
             context.blackHole(array[0])
         }
 
+        benchmark.benchmark(name: "Array SHA512 (10)") { context in
+            var generator = SHA512RandomGenegator()
+
+            var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+            for _ in 0 ..< 1_000_000 {
+                array.shuffle(algorithm: .default, using: &generator)
+            }
+
+            context.blackHole(array[0])
+        }
+
         if #available(macOS 26.0, *) {
             benchmark.benchmark(name: "InlineArray InlineMT19937 (10)") { context in
                 var generator = InlineMT19937RandomGenegator()
@@ -119,6 +131,29 @@ private extension Benchmark {
 
         benchmark.benchmark(name: "Array MT19937 (100)") { context in
             var generator = MT19937RandomGenegator()
+
+            var array = [
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+                51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+                61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+                71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+                81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+                91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
+            ]
+
+            for _ in 0 ..< 1_000_000 {
+                array.shuffle(algorithm: .default, using: &generator)
+            }
+
+            context.blackHole(array[0])
+        }
+
+        benchmark.benchmark(name: "Array SHA512 (100)") { context in
+            var generator = SHA512RandomGenegator()
 
             var array = [
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -191,7 +226,7 @@ private extension Benchmark {
         }
 
         if #available(macOS 26.0, *) {
-            benchmark.benchmark(name: "Faro InlineArrate MT19937 (100)") { context in
+            benchmark.benchmark(name: "Faro InlineArray MT19937 (100)") { context in
                 var generator = InlineMT19937RandomGenegator()
 
                 var array: InlineArray = [
